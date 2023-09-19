579495b1c1a8a5f690ed18ae7502b521463c2d7b
mike <maickjonathan21@gmail.com>
2023-09-19 11:51:30 +0300

    Intial commit
    Adding all files.

---
+++ app.js
@@ -0,0 +1,9 @@
+let menuBtn= document.getElementById('menu');
+let closeBtn=document.getElementById('nav-close');
+let navbar=document.getElementById('navbar');
+menuBtn.addEventListener('click',()=>{
+    navbar.classList.add('oneshaNavbar')
+});
+closeBtn.addEventListener('click',()=>{
+    navbar.classList.remove('oneshaNavbar')
+});
